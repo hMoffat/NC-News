1b7180c74624085af28e1d1ee21cb6843788ac57
hMoffat <hmoffat.work@gmail.com>
2024-01-15 12:18:20 +0000

    Set up test suit, first test and first endpoint.

---
+++ __tests__/app.test.js
@@ -0,0 +1,31 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("/api/snacks", () => {
+  describe("GET /api/snacks", () => {
+    it("Status 200: responds with an array of topic objects containing the slug and description", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          const topics = body.topics;
+          expect(topics.length).toBe(3);
+          topics.forEach((topic) => {
+            expect(topic).toHaveProperty("description", expect.any(String));
+            expect(topic).toHaveProperty("slug", expect.any(String));
+          });
+        });
+    });
+  });
+});
