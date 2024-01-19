cc6abb4c798b99f331140c86273287219856f4c2
hMoffat <hmoffat.work@gmail.com>
2024-01-19 14:51:38 +0000

    Add GET /api/users/:username endpoint and error handle.

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,18 @@
-const { fetchUsers } = require("../models/users.model");
+const { fetchUsers, fetchUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (req, res) => {
   fetchUsers().then((users) => {
     res.status(200).send({ users });
   });
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  fetchUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
