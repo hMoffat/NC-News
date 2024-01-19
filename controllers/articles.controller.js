e376906cefca3b88e7a45f640f7f439c6f04beb8
hMoffat <hmoffat.work@gmail.com>
2024-01-19 12:02:49 +0000

    Add sort_by and order queries with error handling.

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -20,9 +20,9 @@ exports.getArticleById = (req, res, next) => {
 };
 
 exports.getArticles = (req, res, next) => {
-  const { topic } = req.query;
+  const { topic, sort_by, order } = req.query;
 
-  const fetchArticlesQuery = fetchArticles(topic);
+  const fetchArticlesQuery = fetchArticles(topic, sort_by, order);
   const queries = [fetchArticlesQuery];
 
   if (topic) {
