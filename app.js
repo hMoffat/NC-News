63a8b47f3ee584b4bddd90c12ab3d0e98450c746
hMoffat <hmoffat.work@gmail.com>
2024-02-05 09:58:29 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routes/api-router");
 const { getUsers } = require("./controllers/users.controller");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
